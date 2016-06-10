449c335603379c95beba7499895723ff6bad799e
JoeyGinorio <joeyvelez@knights.ucf.edu>
2016-06-10 10:50:02 -0400

    added gridworld implementation

---
+++ gridworld.py
@@ -0,0 +1,16 @@
+# Joey Velez-Ginorio
+# Gridworld Implementation
+# ---------------------------------
+# - Includes BettingGame example
+
+import mdp.MDP
+import numpy
+import random
+
+class GridWorld():
+	"""
+		 Defines a gridworld environment to be solved by an MDP!
+		 Uses the grid example from the Sutton, Barto text.
+	"""
+
+	
